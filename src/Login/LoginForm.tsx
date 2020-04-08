e5445de9b85467167cab778e9ae2a4b9de4f3e68
Arun Manohar Manoharan <amanoharan@trinitypartners.com>
2020-04-08 11:25:31 -0400

    Add validation to login screen

--- src/Login/LoginForm.tsx
+++ src/Login/LoginForm.tsx
@@ -12,12 +12,15 @@ import {
 
 import { Flex, Box } from "reflexbox";
 import { Redirect } from "react-router-dom";
+import AppToaster from '../utils/AppToaster';
 
 const LoginForm: FunctionComponent = () => {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [showPassword, setShowPassword] = useState<boolean>(false);
   const [isSignUp, setSignUp] = useState<boolean>(false);
+  const [isInvalidPassword, setIsInvalidPassword] = useState<boolean>(false);
+  const [isInvalidUsername, setIsInvalidUsername] = useState<boolean>(false);
 
   const handleLockClick = () => setShowPassword(!showPassword);
 
@@ -39,7 +42,23 @@ const LoginForm: FunctionComponent = () => {
   );
 
   const handleSignUp = () => {
-    setSignUp(true);
+      setSignUp(true);
+  };
+
+  const handleLogin = () => {
+    if(!username) {
+      setIsInvalidUsername(true)
+    } else {
+      setIsInvalidUsername(false)
+    }
+    if(!password) {
+      setIsInvalidPassword(true)
+    } else {
+      setIsInvalidPassword(false)
+    }
+    if (username && password) {
+      AppToaster.show({message: 'User logged in.'})
+    }
   };
 
   if (isSignUp) {
@@ -51,20 +70,20 @@ const LoginForm: FunctionComponent = () => {
         <div className="col-lg-5">
           <Card interactive={true} elevation={Elevation.TWO}>
             <div className="p-4 position-relative">
-              <FormGroup label='Username/Email'>
+              <FormGroup label="Username/Email">
                 <InputGroup
-                  intent={Intent.PRIMARY}
+                  intent={isInvalidUsername ? Intent.DANGER : Intent.PRIMARY}
                   value={username}
                   rightElement={userIcon}
                   round
                   onChange={(e: any) => setUsername(e.currentTarget.value)}
                 />
               </FormGroup>
-              <FormGroup label='Password'>
+              <FormGroup label="Password">
                 <InputGroup
                   placeholder="Enter your password"
                   rightElement={lockButton}
-                  intent={Intent.PRIMARY}
+                  intent={isInvalidPassword ? Intent.DANGER : Intent.PRIMARY}
                   type={showPassword ? "text" : "password"}
                   value={password}
                   round
@@ -72,7 +91,7 @@ const LoginForm: FunctionComponent = () => {
                 />
               </FormGroup>
               <FormGroup>
-                <Button intent={Intent.PRIMARY}>Login</Button>
+                <Button intent={Intent.PRIMARY} onClick={handleLogin}>Login</Button>
               </FormGroup>
               <Flex flexWrap="wrap">
                 <Box width={[1, 1 / 2]} p={3}>
